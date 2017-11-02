f23ef33578146b234c75bc9ab20bd5817c335067
Kenneth Torng <ktorng@gmail.com>
2017-11-02 09:14:12 -0500

    (2.1) Remove Duplicates py

---
+++ CrackingTheCodingInterview/ch02-LinkedLists/LinkedList.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __str__(self):
+        arr = []
+        curr = self.head
+
+        while curr:
+            arr.append(curr.value)
+            curr = curr.next
+
+        return str(arr)
+
+    def add(self, value):
+        node = Node(value)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+    # Remove first node that matches value
+    def remove(self, value):
+        curr = self.head
+
+        if curr.value == value:
+            self.head = self.head.next
+        else:
+            while curr.next:
+                if curr.next.value == value:
+                    curr.next = curr.next.next
+                    break
+                else:
+                    curr = curr.next
---
+++ CrackingTheCodingInterview/ch02-LinkedLists/remove_duplicates.py
@@ -0,0 +1,30 @@
+from LinkedList import *
+
+
+# O(n) time
+# O(n) space
+def remove_duplicates(ll):
+    curr = ll.head
+    visited = set()
+
+    while curr.next:
+        visited.add(curr.value)
+        if curr.next.value in visited:
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+
+    return ll
+
+
+a = LinkedList()
+a.add(5)
+a.add(3)
+a.add(3)
+a.add(8)
+a.add(2)
+a.add(5)
+a.add(8)
+print(a)
+remove_duplicates(a)
+print(a)
